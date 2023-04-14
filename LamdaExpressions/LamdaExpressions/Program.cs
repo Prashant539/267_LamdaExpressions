87376e364ae30c60b093a3873aca5079c40d3ec3
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-04-14 23:24:49 +0530

    [Prashant]Add: Create a list of student data management

---
+++ LamdaExpressions/LamdaExpressions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace LamdaExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Student data Management");
+            Student student1 = new Student();  
+
+            List<Student> list = new List<Student>()
+            {
+                new Student() { Id = 1, Name = "Prashant", PhoneNumber = 9820899968, Address = "Gorakhpur", Age = 25, TotalMarks=95},
+                new Student() { Id = 2, Name = "Rupesh", PhoneNumber = 7746039890, Address = "Pune", Age = 27, TotalMarks=50},
+                new Student() { Id = 3, Name = "Vishnu", PhoneNumber = 7756039898, Address = "Kerla", Age = 30, TotalMarks=105},
+                new Student() { Id = 4, Name = "Akhil", PhoneNumber = 9983084421, Address = "Ashok Nagar", Age = 35, TotalMarks=100},
+
+            };
+            DisplayStudentDetails(list);
+            Console.ReadLine();
+        }
+        public static void DisplayStudentDetails(List<Student> list)
+        {
+            Console.WriteLine("Displaying All Students");
+            foreach (Student student in list)
+            {
+                Console.WriteLine("ID :" + student.Id + "  " + "Name :" + student.Name + "  " + "Phone Number :" + student.PhoneNumber + "  " + "Address :" + student.Address + "  " + "Age :" + student.Age + " " + "TotalMarks : " + student.TotalMarks);
+
+            }
+        }
+    }
+}
+
+           
+        
+
---
+++ LamdaExpressions/LamdaExpressions/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LamdaExpressions
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public long PhoneNumber { get; set; }
+        public int TotalMarks { get; set; }
+
+    }
+    
+}
+
